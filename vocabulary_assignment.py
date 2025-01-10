b611eea0cec05c3eb704dde79fb28806215888f5
RomulusLab <126527988+RomulusLab@users.noreply.github.com>
2025-01-09 19:48:57 -0500

    Add files via upload

---
+++ vocabulary_assignment.py
@@ -0,0 +1,71 @@
+"""Functions for creating, transforming, and adding prefixes to strings."""
+
+
+def add_prefix_un(word):
+    """Take the given word and add the 'un' prefix.
+
+    :param word: str - containing the root word.
+    :return: str - of root word prepended with 'un'.
+    """
+
+    negate_word = 'un'+word
+    return negate_word
+
+
+def make_word_groups(vocab_words):
+    """Transform a list containing a prefix and words into a string with the prefix followed by the words with prefix prepended.
+
+    :param vocab_words: list - of vocabulary words with prefix in first index.
+    :return: str - of prefix followed by vocabulary words with
+            prefix applied.
+
+    This function takes a `vocab_words` list and returns a string
+    with the prefix and the words with prefix applied, separated
+     by ' :: '.
+
+    For example: list('en', 'close', 'joy', 'lighten'),
+    produces the following string: 'en :: enclose :: enjoy :: enlighten'.
+    """
+
+    prefix = vocab_words[0]
+    prefix_append = [prefix + word for word in vocab_words[1:]]
+
+    return prefix + ' :: ' + ' :: '.join(prefix_append)
+
+def remove_suffix_ness(word):
+    """Remove the suffix from the word while keeping spelling in mind.
+
+    :param word: str - of word to remove suffix from.
+    :return: str - of word with suffix removed & spelling adjusted.
+
+    For example: "heaviness" becomes "heavy", but "sadness" becomes "sad".
+    """
+
+    remove_ness = word[0:-4]
+    remove_i = remove_ness[0:-1]+'y'
+
+    if remove_ness[-1] == 'i':
+        return remove_i
+    else:
+        return remove_ness
+
+
+def adjective_to_verb(sentence, index):
+    """Change the adjective within the sentence to a verb.
+
+    :param sentence: str - that uses the word in sentence.
+    :param index: int - index of the word to remove and transform.
+    :return: str - word that changes the extracted adjective to a verb.
+
+    For example, ("It got dark as the sun set.", 2) becomes "darken".
+    """
+
+    extracted_word = sentence.split()[index]
+    word_transformed = extracted_word+'en'
+    avoid_period = extracted_word[0:-1]+'en'
+
+    if index == -1:
+        return avoid_period
+    else: 
+        return word_transformed
+    
